a6d943aab2fc8bc3ec898ec8b72018d1e9a20eb5
oueya1479 <68142821+oueya1479@users.noreply.github.com>
2020-11-22 01:13:17 +0900

    commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const {User} = require('./models/User')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded)({extended: true})
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://oueya1479:Kimdongheon316!@myapp.rq6tl.mongodb.net/myapp?retryWrites=true&w=majority', {
@@ -16,3 +21,12 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
+app.post('/register'), (req, res) => {
+  const user = new User(req.body)
+  user.save((err, doc) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+}
