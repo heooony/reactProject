8217705a61fb94a6ea5b6d3e4f0f5071adacbd6a
oueya1479 <68142821+oueya1479@users.noreply.github.com>
2020-11-22 02:17:14 +0900

    commit perfect

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const {User} = require('./models/User');
 const bodyParser = require('body-parser');
+const User = require("./models/User")
 
-app.use(bodyParser.urlencoded)({extended: true})
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
@@ -23,7 +23,7 @@ app.listen(port, () => {
 
 app.post('/register', (req, res) => {
   const user = new User(req.body)
-  user.save((err, doc) => {
+  user.save((err, userInfo) => {
     if(err) return res.json({success: false, err})
     return res.status(200).json({
       success: true
