7d6198a0b790e31f100eaca85a76181fdb2142ea
oueya1479 <68142821+oueya1479@users.noreply.github.com>
2020-11-23 00:12:18 +0900

    compare password

--- index.js
+++ index.js
@@ -30,3 +30,30 @@ app.post('/register', (req, res) => {
     })
   })
 })
+
+app.post('/login', (req, res) => {
+  //email 확인
+  User.findOne({email: req.body.email}, (err, user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+    //email-password 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch) return res.json({
+        loginSucess: false, 
+        message: "비밀번호가 틀렸습니다."
+      })
+      //token생성
+      user.generateToken((err, user) => {
+
+      })
+    })
+
+  })
+  
+
+  
+})
